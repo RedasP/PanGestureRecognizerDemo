0fed3222bf6f8ceacb1bd0bd685ce05a6493673f
Reed <rpaliskevicius@gmail.com>
2019-02-06 23:38:59 +0200

    Initial commit

---
+++ PanPlayground/PanPlayground/CustomViewCell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PanPlayground
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomViewCell : ViewCell
+    {
+        public CustomViewCell()
+        {
+            InitializeComponent();
+        }
+
+        private void PanGestureRecognizer_OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            
+            Debug.WriteLine($"{e.StatusType} {e.TotalX} {e.TotalY}");
+        }
+    }
+}
---
+++ PanPlayground/PanPlayground/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PanPlayground
+{
+
+    public class User
+    {
+        public string Name { get; set; }
+    }
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Users.ItemsSource = new List<User>()
+            {
+                new User() {Name = "123"}
+            };
+        }
+    }
+}
